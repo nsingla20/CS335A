90af7e18705d04b56c3e664e1542f5c0b8bb1c30
nsingla20 <nsingla20@iitk.ac.in>
2023-04-21 11:51:39 +0530

    tests up

--- milestone4/tests/test_8.java
+++ milestone4/tests/test_8.java
@@ -1,60 +1,57 @@
 public class HeapSort {
-	public void sort(int arr[])
-	{
-		int n = arr.length;
-		for (int i = n / 2 - 1; i >= 0; i--)
-			heapify(arr, n, i);
-		for (int i = n - 1; i >= 0; i--) {
-			int temp = arr[0];
-			arr[0] = arr[i];
-			arr[i] = temp;
-			heapify(arr, i, 0);
-		}
-	}
-	void heapify(int arr[], int n, int i)
+
+	void heapify(int arr, int n, int i)
 	{
 		int largest = i; // Initialize largest as root
 		int l = 2 * i + 1; // left = 2*i + 1
 		int r = 2 * i + 2; // right = 2*i + 2
 
 		// If left child is larger than root
-		if (l < n && arr[l] > arr[largest])
+		if (l < n)
 			largest = l;
 
 		// If right child is larger than largest so far
-		if (r < n && arr[r] > arr[largest])
+		if (r < n)
 			largest = r;
 
 		// If largest is not root
 		if (largest != i) {
-			int swap = arr[i];
-			arr[i] = arr[largest];
-			arr[largest] = swap;
+			// int swap = arr[i];
+			// arr[i] = arr[largest];
+			// arr[largest] = swap;
 
 			// Recursively heapify the affected sub-tree
 			heapify(arr, n, largest);
 		}
 	}
-
-	/* A utility function to print array of size n */
-	static void printArray(int arr[])
+	public void sort(int arr)
 	{
-		int n = arr.length;
+		int n = arr;
+		for (int i = n / 2 - 1; i >= 0; i--)
+			heapify(arr, n, i);
+		for (int i = n - 1; i >= 0; i--) {
+			heapify(arr, i, 0);
+		}
+	}
+	/* A utility function to print array of size n */
+	static void printArray(int arr)
+	{
+		int n = arr;
 		for (int i = 0; i < n; ++i)
-			System.out.print(arr[i] + " ");
+			System.out.println(arr);
 		System.out.println();
 	}
 
 	// Driver program
 	public static void main(String args[])
 	{
-		int arr[] = { 12, 11, 13, 5, 6, 7 };
-		int n = arr.length;
+		int arr = 12;
+		int n = arr;
 
-		HeapSort ob = new HeapSort();
-		ob.sort(arr);
+		HeapSort ob;
+		HeapSort.sort(arr);
 
-		System.out.println("Sorted array is");
+		System.out.println(1);
 		printArray(arr);
 	}
 }
